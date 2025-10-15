7eae8602e56526bd423e95ec1898c8a6802dcc53
CuzImSlymi <fridolinojustin@gmail.com>
2025-10-15 19:14:16 +0200

    chore: bump version to 0.5.0

--- puter/__init__.py
+++ puter/__init__.py
@@ -2,6 +2,6 @@ from .ai import PuterAI
 from .exceptions import PuterError, PuterAuthError, PuterAPIError
 from .config import config, PuterConfig
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __all__ = ["PuterAI", "PuterError", "PuterAuthError", "PuterAPIError", "config", "PuterConfig"]
 
